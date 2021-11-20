aa0e1dc9a590d6e7c674679188c7447a61a6e539
christiansc96 <cdsanchezc96@gmail.com>
2021-11-19 20:55:18 -0600

    add permission in android manifest
    add permission in android manifest

--- devfestmexico/lib/home/repository/home-api-repository.dart
+++ devfestmexico/lib/home/repository/home-api-repository.dart
@@ -4,7 +4,7 @@ class HomeApiRepository {
   final String api = "https://rickandmortyapi.com/api/";
 
   Future<http.Response> getCharacters() async {
-    Uri uri = Uri.parse('${api}character');
+    Uri uri = Uri.parse('${api}character/');
     return await http.get(uri, headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
     });
